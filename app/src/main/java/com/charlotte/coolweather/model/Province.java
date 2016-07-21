7debcbed59a3d76ba9d77bdcd0b887aa261dd345
charlotte <1515774621@qq.com>
2016-07-21 10:12:32 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/charlotte/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.charlotte.coolweather.model;
+
+/**
+ * Created by Administrator on 2016/7/20 0020.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
