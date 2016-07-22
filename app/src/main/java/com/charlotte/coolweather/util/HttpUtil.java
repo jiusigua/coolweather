3b98ed567373f117772b4d9523ba85b12a182082
charlotte <1515774621@qq.com>
2016-07-22 11:29:03 +0800

    修改程序图标和名称。

--- app/src/main/java/com/charlotte/coolweather/util/HttpUtil.java
+++ app/src/main/java/com/charlotte/coolweather/util/HttpUtil.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
@@ -26,7 +25,7 @@ public class HttpUtil {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(in));
                     StringBuilder response = new StringBuilder();
                     String line;
-                    while((line = reader.readLine()) != null)){
+                    while((line = reader.readLine()) != null){
                         response.append(line);
                     }
                     if (listener != null){
