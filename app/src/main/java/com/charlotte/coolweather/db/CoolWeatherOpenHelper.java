d00944623806684ccc0fa25b7c3c0a6944371365
charlotte <1515774621@qq.com>
2016-07-27 01:44:05 +0800

    第二次修改。

--- app/src/main/java/com/charlotte/coolweather/db/CoolWeatherOpenHelper.java
+++ app/src/main/java/com/charlotte/coolweather/db/CoolWeatherOpenHelper.java
@@ -22,7 +22,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper{
     public static final String CREATE_CITY = "create table City ("
              + "id integer primary key autoincrement,"
              + "city_name text,"
-             + "city_code text)"
+             + "city_code text,"
              + "province_id integer)";
      /**
      * County表建表语句
